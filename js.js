da51dbcdaeec46bd2e43497618e96751704bb517
JuliRavasi1 <juli.ravasi00@gmail.com>
2022-02-28 20:48:49 +0100

    desafio miercoles 29/2

--- js.js
+++ js.js
@@ -2,7 +2,6 @@ let pregunta = true
 let monto = 0
 let cantidad
 
-
 function numeroVal(numero) {
     while (numero <= 0 || isNaN(numero)) {
         numero = parseInt(prompt("Numero invalido, digite nuevamente un numero"))
@@ -10,19 +9,18 @@ function numeroVal(numero) {
     return numero;
 }
 
-function operacion() {
-    let opc
-    let contador = 0
-    do {
-        if (contador != 0) {
-            alert("Numero invalido")
-        }
-        opc = parseInt(prompt("OPERACIONES:\n1.Ingresar mas dinero \n2.Ingresar mas Gastos \n3.Finalizar \nDigite el numero de la operacion que desea hacer:"))
+const operaciones = [
+    "1.Ingresar mas dinero\n",
+    "2.Ingresar mas gastos\n",
+    "3.Finalizar\n"
+];
 
-        contador++
-    } while (isNaN(opc) || opc > 3 || opc < 1)
-    return opc
+let operacionSeleccionada
+let lista = " ";
+operaciones.forEach((opcion)=>{
+    lista+=opcion;
 }
+);
 
 function ingresarMonto(){
     let nuevo = parseInt(prompt("Digite el monto que va ingresar"))
@@ -33,7 +31,6 @@ function ingresarMonto(){
 function ingresargasto(){
     cantidad = parseInt(prompt("Digite la cantidad de gastos"))
     cantidad = numeroVal(cantidad)
-
     let totalGasto = 0
     let gasto
     for (let i = 1; i <= cantidad; i++) {
@@ -42,19 +39,22 @@ function ingresargasto(){
         totalGasto = totalGasto + gasto
     }
     monto = monto - totalGasto
-    
 }
 
-
 let inicio = true
+let inicio2 = true
+
 do {
     if(inicio != false){
         ingresarMonto()
         ingresargasto()
         alert("El monto disponible ahora es: " + monto)
         inicio = false
-    }
-    switch (operacion()) {
+        
+    }else if(inicio2 != false){
+    operacionSeleccionada = parseInt(prompt("Seleccione una opción:\n"+lista));
+    operacionSeleccionada = numeroVal (operacionSeleccionada);
+    switch (operacionSeleccionada) {
         case 1:
             ingresarMonto()
             alert("El nuevo monto disponible es: " + monto)
@@ -67,63 +67,5 @@ do {
             alert("Gracias!!")
             pregunta = false
     }
-
-} while (pregunta == true)
-
-
-
-
-// ------------- otra manera de hacerlo ------------------
-
-
-/*
-AHORROS 
-
-1. coloque monto total
-2. indique cuantos gastos fijos tendra este mes
-3. coloque monto de cada gasto fijo (gasto 1, 2, 3, etc)
-4. esto te queda para ahorrar 
-5. en caso que este, sea cero, quiere ingresar nuevamente un monto total? 
-5.1 si (reinicia todo)
-5.1 no (fin) 
-*/
-
-// let monto = parseInt(prompt("Ingrese monto total disponible"));
-// let contador = parseInt(prompt("Ingrese cuántos gastos fijos tendrá este mes"));
-// let gastoTotal = 0;
-
-// if (!isNaN (monto) && !isNaN(contador) || contador >= 1){
-//     for (let i=0; i<contador; i++){
-//         let gastos = parseInt(prompt("Ingresa monto de gastos fijos"))
-//         gastoTotal = gastoTotal+gastos;
-//     }
-//     monto = monto-gastoTotal;
-//     alert ("Su monto disponible para ahorrar es de: " + monto + "$");
-// } else{
-//     alert ("Monto Invalido");
-// } 
-
-// while (monto === 0) {
-//     let respuesta = prompt("Quiere ingresar nuevo monto total disponible?: \n SI \n NO");
-//     if (respuesta === "SI"){
-//         alert ("La respuesta fue: SI");
-//         monto = parseInt(prompt("Ingrese monto total disponible"));
-//         contador = parseInt(prompt("Ingrese cuántos gastos fijos tendrá este mes"));
-//         if (!isNaN (monto) && !isNaN(contador) || contador >= 1){
-//             for (let i=0; i<contador; i++){
-//                 let gastos = parseInt(prompt("Ingresa monto de gastos fijos"))
-//                 gastoTotal = gastoTotal+gastos;
-//             }
-//             monto = monto-gastoTotal;
-//             alert ("Su monto disponible para ahorrar es de: " + monto + "$");
-//         } else{
-//             alert ("Monto Invalido");
-//         } 
-//     } else if (respuesta === "NO"){
-//         alert ("gracias");
-//         monto = 1; 
-//     }
-// }
-
-
-
+}
+}while (pregunta == true)
